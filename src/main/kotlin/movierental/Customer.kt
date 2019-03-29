05c9e32fdb05e39f7afffe6a536b1c6ec60cc751
aquaraga <aquaraga@gmail.com>
2019-03-29 14:58:32 +0530

    Replace temp with query - 2

--- src/main/kotlin/movierental/Customer.kt
+++ src/main/kotlin/movierental/Customer.kt
@@ -10,23 +10,32 @@ class Customer(val name: String) {
     }
 
     fun statement(): String {
-        var totalAmount = 0.0
-        var frequentRenterPoints = 0
         var result = "Rental Record for $name\n"
-
         for (each in _rentals) {
-
-            frequentRenterPoints += each.getFrequentRenterPoints()
-
             // show figures for this rental
             result += "\t" + each.movie.title + "\t" + each.getCharge().toString() + "\n"
-            totalAmount += each.getCharge()
         }
-
         // add footer lines
-        result += "Amount owed is " + totalAmount.toString() + "\n"
-        result += "You earned " + frequentRenterPoints.toString() + " frequent renter points"
+        result += "Amount owed is " + totalCharge().toString() + "\n"
+        result += "You earned " + totalFrequentRenterPoints().toString() + " frequent renter points"
 
         return result
+    }
+
+    private fun totalCharge(): Double {
+        var totalAmount = 0.0
+        for (each in _rentals) {
+            totalAmount += each.getCharge()
+        }
+        return totalAmount
+    }
+
+    private fun totalFrequentRenterPoints(): Int {
+        var frequentRenterPoints = 0
+        for (each in _rentals) {
+            frequentRenterPoints += each.getFrequentRenterPoints()
+        }
+        return frequentRenterPoints
+
     }
 }
